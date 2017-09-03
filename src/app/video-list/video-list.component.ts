13719f2e649cc814847bbcb574d8426c87edf75d
KasunGunarathna <kasunpradeepa.gunarathna@gmail.com>
2017-09-03 14:04:27 +0530

    selectVideo event

--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'video-list',
   templateUrl: './video-list.component.html',
   styleUrls: ['./video-list.component.css'],
-  inputs:['videos']
+  inputs:['videos'],
+  outputs:['SelectVideo']
 })
 export class VideoListComponent implements OnInit {
-
+  public SelectVideo=new EventEmitter();
   constructor() { }
 
   ngOnInit() {
